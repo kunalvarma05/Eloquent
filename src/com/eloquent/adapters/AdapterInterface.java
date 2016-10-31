eeceaf0de2da00bdfea27acf0a8c91db7ba5805b
Kunal Varma <kunalvarma05@gmail.com>
2016-10-31 06:06:42 +0530

    AdapterInterface

---
+++ src/com/eloquent/adapters/AdapterInterface.java
@@ -0,0 +1,125 @@
+package com.eloquent.adapters;
+
+import com.eloquent.exceptions.EloquentException;
+import com.eloquent.models.File;
+
+public interface AdapterInterface {
+
+    /**
+     * Read a file
+     *
+     * @param path File Path
+     *
+     * @return {@link File}
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    File read(String path) throws EloquentException;
+
+    /**
+     * Write a new file.
+     *
+     * @param path File Path
+     * @param contents File contents
+     *
+     * @return {@link File}
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    File write(String path, String contents) throws EloquentException;
+
+    /**
+     * Update a file.
+     *
+     * @param path File Path
+     * @param contents File contents
+     *
+     * @return {@link File}
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    File update(String path, String contents) throws EloquentException;
+
+    /**
+     * Create or Update a file.
+     *
+     * @param path File Path
+     * @param contents File contents
+     *
+     * @return {@link File}
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    File put(String path, String contents) throws EloquentException;
+
+    /**
+     * Check if a File exists or not
+     *
+     * @param path File Path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean has(String path) throws EloquentException;
+
+    /**
+     * Rename a file
+     *
+     * @param path File Path
+     * @param newPath New path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean rename(String path, String newPath) throws EloquentException;
+
+    /**
+     * Copy a file
+     *
+     * @param path File Path
+     * @param newPath New path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean copy(String path, String newPath) throws EloquentException;
+
+    /**
+     * Delete a file
+     *
+     * @param path File Path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean delete(String path) throws EloquentException;
+
+    /**
+     * Delete a directory
+     *
+     * @param path File Path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean deleteDir(String path) throws EloquentException;
+
+    /**
+     * Create a directory
+     *
+     * @param path File Path
+     *
+     * @return boolean
+     *
+     * @throws EloquentException Eloquent Exception
+     */
+    boolean createDir(String path) throws EloquentException;
+
+
+
+}
