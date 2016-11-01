ea20dca1112af2ae999f774e799fb087d11d0943
Kunal Varma <kunalvarma05@gmail.com>
2016-11-01 17:36:23 +0530

    Setup Gulp Tasks

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', function() {
+    gulp.src('./sass/*.scss')
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
