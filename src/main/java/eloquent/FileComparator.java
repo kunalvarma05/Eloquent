3a727d8a9733b2fa52606c64069c277347551e8b
Kunal Varma <kunalvarma05@gmail.com>
2016-11-20 16:33:25 +0530

    FileComparator javadoc

--- src/main/java/eloquent/FileComparator.java
+++ src/main/java/eloquent/FileComparator.java
@@ -6,6 +6,11 @@ import java.util.Comparator;
 
 /**
  * @author Kunal
+ * @apiNote Usage:
+ *
+ * Sort by name: Collections.sort(files, new FileComparator());
+ * Sort by size: Collections.sort(files, new FileComparator("size"));
+ * Sort by timestamp: Collections.sort(files, new FileComparator("timestamp"));
  */
 public class FileComparator implements Comparator<File> {
 
@@ -33,10 +38,26 @@ public class FileComparator implements Comparator<File> {
         }
     }
 
+    /**
+     * Sort Files by name
+     *
+     * @param f1 File
+     * @param f2 File
+     *
+     * @return int
+     */
     protected int compareByName(File f1, File f2) {
         return f1.getName().compareTo(f2.getName());
     }
 
+    /**
+     * Sort Files by size
+     *
+     * @param f1 File
+     * @param f2 File
+     *
+     * @return int
+     */
     protected int compareBySize(File f1, File f2) {
         int s1 = Integer.parseInt(f1.getSize());
         int s2 = Integer.parseInt(f2.getSize());
@@ -44,6 +65,14 @@ public class FileComparator implements Comparator<File> {
         return s1 - s2;
     }
 
+    /**
+     * Sort Files by timestamp
+     *
+     * @param f1 File
+     * @param f2 File
+     *
+     * @return int
+     */
     protected int compareByTimestamp(File f1, File f2) {
         return f1.getTimestamp().compareTo(f2.getTimestamp());
     }
