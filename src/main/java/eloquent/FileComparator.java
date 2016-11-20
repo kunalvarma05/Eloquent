4ba109d12ddca086496a1ba7c2eb44ec64b89d82
Kunal Varma <kunalvarma05@gmail.com>
2016-11-20 19:57:05 +0530

    FileComparator sort optimiziation

--- src/main/java/eloquent/FileComparator.java
+++ src/main/java/eloquent/FileComparator.java
@@ -1,6 +1,5 @@
 package eloquent;
 
-import eloquent.models.File;
 import eloquent.models.Metadata;
 
 import java.util.Comparator;
@@ -9,85 +8,100 @@ import java.util.Comparator;
  * @author Kunal
  * @apiNote Usage:
  *
- * Sort by name: Collections.sort(files, new FileComparator());
- * Sort by size: Collections.sort(files, new FileComparator("size"));
- * Sort by timestamp: Collections.sort(files, new FileComparator("timestamp"));
+ *          Sort by name: Collections.sort(files, new FileComparator()); Sort by
+ *          size: Collections.sort(files, new FileComparator("size")); Sort by
+ *          timestamp: Collections.sort(files, new FileComparator("timestamp"));
  */
 public class FileComparator implements Comparator<Metadata> {
 
-    private static final String DEFAULT_COMPARISON_KEY = "name";
+	private static final String DEFAULT_COMPARISON_KEY = "name";
+	protected final String ascKey = "asc";
+	protected final String descKey = "desc";
+	protected final String defaultOrder = ascKey;
+	protected String key;
+	protected String order;
 
-    protected String key;
-    protected String order;
+	public FileComparator() {
+		this.key = DEFAULT_COMPARISON_KEY;
+	}
 
-    protected final String ascKey = "asc";
-    protected final String descKey = "desc";
-    protected final String defaultOrder = ascKey;
+	public FileComparator(String key) {
+		this.key = key;
+		this.order = defaultOrder;
+	}
 
-    public FileComparator() {
-        this.key = DEFAULT_COMPARISON_KEY;
-    }
+	public FileComparator(String key, String order) {
+		this.key = key;
+		this.order = order;
+	}
 
-    public FileComparator(String key) {
-        this.key = key;
-        this.order = defaultOrder;
-    }
+	public int compare(Metadata f1, Metadata f2) {
 
-    public FileComparator(String key, String order) {
-        this.key = key;
-        this.order = order;
-    }
+		switch (this.key) {
+		case "size":
+			return this.compareBySize(f1, f2);
+		case "timestamp":
+			return this.compareByTimestamp(f1, f2);
+		default:
+			return this.compareByName(f1, f2);
+		}
+	}
 
-    public int compare(Metadata f1, Metadata f2) {
-
-        switch (this.key) {
-            case "size":
-                return this.compareBySize(f1, f2);
-            case "timestamp":
-                return this.compareByTimestamp(f1, f2);
-            default:
-                return this.compareByName(f1, f2);
+	/**
+	 * Sort Files by name
+	 *
+	 * @param f1
+	 *            File
+	 * @param f2
+	 *            File
+	 *
+	 * @return int
+	 */
+	protected int compareByName(Metadata f1, Metadata f2) {
+        if (f1.getName() != null && f2.getName() != null) {
+            return f1.getName().compareTo(f2.getName());
         }
-    }
 
-    /**
-     * Sort Files by name
-     *
-     * @param f1 File
-     * @param f2 File
-     *
-     * @return int
-     */
-    protected int compareByName(Metadata f1, Metadata f2) {
-        return f1.getName().compareTo(f2.getName());
-    }
+        return -1;
+	}
 
-    /**
-     * Sort Files by size
-     *
-     * @param f1 File
-     * @param f2 File
-     *
-     * @return int
-     */
-    protected int compareBySize(Metadata f1, Metadata f2) {
+	/**
+	 * Sort Files by size
+	 *
+	 * @param f1
+	 *            File
+	 * @param f2
+	 *            File
+	 *
+	 * @return int
+	 */
+	protected int compareBySize(Metadata f1, Metadata f2) {
+        if (f1.getSize() != null && f2.getSize() != null) {
+            int s1 = Integer.parseInt(f1.getSize().isEmpty() ? "0" : f1.getSize());
+            int s2 = Integer.parseInt(f2.getSize().isEmpty() ? "0" : f2.getSize());
 
-        int s1 = Integer.parseInt(f1.getSize());
-        int s2 = Integer.parseInt(f2.getSize());
+            return s1 - s2;
+        }
 
-        return s1 - s2;
-    }
+        return -1;
+	}
 
-    /**
-     * Sort Files by timestamp
-     *
-     * @param f1 File
-     * @param f2 File
-     *
-     * @return int
-     */
-    protected int compareByTimestamp(Metadata f1, Metadata f2) {
-        return f1.getTimestamp().compareTo(f2.getTimestamp());
-    }
+	/**
+	 * Sort Files by timestamp
+	 *
+	 * @param f1
+	 *            File
+	 * @param f2
+	 *            File
+	 *
+	 * @return int
+	 */
+	protected int compareByTimestamp(Metadata f1, Metadata f2) {
+		if (f1.getTimestamp() != null && f2.getTimestamp() != null) {
+			return f1.getTimestamp().compareTo(f2.getTimestamp());
+		}
+
+		return -1;
+	}
 
 }
