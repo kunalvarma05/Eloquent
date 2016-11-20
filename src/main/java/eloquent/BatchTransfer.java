1ecb40866a2803a054f187165dc1991d6fa12c88
Kunal Varma <kunalvarma05@gmail.com>
2016-11-21 01:32:33 +0530

    Use BatchTransferFile for transferring file and allow new path

--- src/main/java/eloquent/BatchTransfer.java
+++ src/main/java/eloquent/BatchTransfer.java
@@ -1,17 +1,18 @@
 package eloquent;
 
+import eloquent.adapters.AdapterInterface;
+import eloquent.exceptions.EloquentException;
+import eloquent.models.BatchTransferFile;
+import eloquent.models.File;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.List;
-
-import eloquent.exceptions.EloquentException;
-import eloquent.models.File;
-import eloquent.adapters.AdapterInterface;
+import java.util.Queue;
 
 public class BatchTransfer {
 
-	protected Queue<String> paths;
+	protected Queue<BatchTransferFile> paths;
 
 	protected List<File> files;
 
@@ -21,7 +22,7 @@ public class BatchTransfer {
 
 	public BatchTransfer() {
 
-		this.paths = new LinkedList<>();
+		this.paths = new LinkedList<BatchTransferFile>();
 		this.files = new ArrayList<>();
 	}
 
@@ -34,7 +35,11 @@ public class BatchTransfer {
 	}
 
 	public BatchTransfer add(String path) {
-		this.paths.add(path);
+		return this.add(path, path);
+	}
+
+	public BatchTransfer add(String path, String newPath) {
+		this.paths.add(new BatchTransferFile(path, newPath));
 
 		return this;
 	}
@@ -56,10 +61,10 @@ public class BatchTransfer {
 		while (!this.paths.isEmpty()) {
 
 			// Retrieve first path | HEAD
-			String path = this.paths.poll();
+			BatchTransferFile transferFile = this.paths.poll();
 
-			Transfer transfer = new Transfer(path);
-			transfer.from(this.fromAdapter).to(this.toAdapter);
+			Transfer transfer = new Transfer(transferFile.getPath());
+			transfer.from(this.fromAdapter).to(this.toAdapter).at(transferFile.getNewpath());
 
 			try {
 				File file = transfer.getFile();
